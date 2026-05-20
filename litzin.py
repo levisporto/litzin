e88da395914fb59df2e75ea37c9e6896c468ac97
Levi S <levisporto14@gmail.com>
2026-05-19 23:53:50 -0300

    now working

--- litzin.py
+++ litzin.py
@@ -2,21 +2,41 @@ import streamlit as st
 from playwright.sync_api import sync_playwright
 import re
 
-def run_scraper(url):
-    with sync_playwright() as p:
+st.title("Digital College - Eventos Data Analytics")
+
+
+with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
-        page.goto(url)
+        page.goto('https://www.sympla.com.br/produtor/digitalcollegebr')
         todos_os_eventos = page.locator('a').all_inner_texts() 
+        links_locator = page.locator("a")
+        all_links = links_locator.evaluate_all("elements => elements.map(e => e.href)")
+        busca = re.compile(r"dados|python|analista|análise|data|analysis|analyst", re.IGNORECASE)
+        evento_list = []
+        link_list = []
+
+        for resultados in todos_os_eventos:
+            if busca.search(resultados):
+                evento_list.append(resultados)
+        
+
+        for resultados in all_links:
+            if busca.search(resultados):
+                link_list.append(resultados)
+            
+        for evento, link in zip(evento_list, link_list):
+            st.subheader(f"📅 {evento}")
+            st.write(f"Link: {link}")
+            st.write("---") # Optional: adds a line between results
+                
+        
+        
         browser.close()
-        return todos_os_eventos
+        
+        
 
-st.title("Digital College - Eventos Data Analytics")
-result = run_scraper('https://www.sympla.com.br/produtor/digitalcollegebr')
 
-busca = re.compile(r"dados|python|analista|análise|data|analysis|analyst", re.IGNORECASE)
 
-for resultados in result:
-    if busca.search(resultados):
-        st.write(f"{resultados}")
+
     
