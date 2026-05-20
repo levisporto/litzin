005c45b30155ebbc45f4a38b33c8df362b7696e1
Levi S <levisporto14@gmail.com>
2026-05-20 01:02:41 -0300

    deu certo!!!

--- litzin.py
+++ litzin.py
@@ -3,19 +3,24 @@ from playwright.sync_api import sync_playwright
 import re
 
 st.title("Digital College - Eventos Data Analytics")
-
+st.write('por Levi S. Porto :)')
 
 with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto('https://www.sympla.com.br/produtor/digitalcollegebr')
+        
         todos_os_eventos = page.locator('a').all_inner_texts() 
         links_locator = page.locator("a")
         all_links = links_locator.evaluate_all("elements => elements.map(e => e.href)")
         busca = re.compile(r"dados|python|analista|análise|data|analysis|analyst", re.IGNORECASE)
+
+        all_imgs = page.get_by_role("link", name=busca).locator("img")
+        image_list = all_imgs.evaluate_all("elements => elements.map(e => e.src)")
+
         evento_list = []
         link_list = []
-        image_list = "https://images.sympla.com.br/69f27ae454bd2-xs.png"
+        
         for resultados in todos_os_eventos:
             if busca.search(resultados):
                 evento_list.append(resultados)
@@ -25,8 +30,8 @@ with sync_playwright() as p:
             if busca.search(resultados):
                 link_list.append(resultados)
             
-        for evento, link in zip(evento_list, link_list, image_list):
-            st.image(image_list)    
+        for evento, link, image in zip(evento_list, link_list, image_list):   
+            st.image({image})
             st.subheader(f"📅 {evento}")
             st.write(f"Link: {link}")
             st.write("---") # Optional: adds a line between results
--- localzin.py
+++ localzin.py
@@ -1,19 +1,11 @@
 from playwright.sync_api import sync_playwright
 import re
 
-def run_scraper(url):
-    with sync_playwright() as p:
+
+with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
-        page.goto(url)
-        all_links = page.locator('a')
-        all_h3_texts = page.locator('h3').all_inner_texts()
-        r = re.compile(r".*(dados|python)", re.IGNORECASE)
-        filtered_h3_texts = list(filter(r.match, all_h3_texts)) 
-        print(all_links)
-        browser.close()
-        return filtered_h3_texts
-
-
-result = run_scraper('https://www.sympla.com.br/produtor/digitalcollegebr')
-print(f"{result}")
+        page.goto('https://www.sympla.com.br/produtor/digitalcollegebr')
+        python_imgs_locator = page.get_by_role("link", name=re.compile("python", re.IGNORECASE)).locator("img")
+        python_srcs = python_imgs_locator.evaluate_all("elements => elements.map(e => e.src)")
+        print(python_srcs)
